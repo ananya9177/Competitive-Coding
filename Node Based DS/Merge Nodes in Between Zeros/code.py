90f8fa9374bd293d0bc2c4854ce924734888395b
ananya9177 <151428838+ananya9177@users.noreply.github.com>
2024-03-15 13:54:16 +0530

    Create code.py

---
+++ Node Based DS/Merge Nodes in Between Zeros/code.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #temp=head
+       
+        #if temp.val==0:
+            #while temp.next.val!=0:
+                #temp.val+=temp.next.val
+               # temp.next=temp.next.next
+        
+        #return head
+        temp=head
+        while temp.next:
+            prev=temp
+            if temp.next.val!=0:
+                temp.val+=temp.next.val
+                temp.next=temp.next.next
+            else:
+                temp=temp.next
+        prev.next=None
+        return head
